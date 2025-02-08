019eb0fc9a300acf03643cf3caf31437ea4fff94
Asiel <143839022+AsielElaouare@users.noreply.github.com>
2025-02-08 20:50:09 +0100

    Add files via upload

---
+++ app/views/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <title>MVC Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+ <main>
+   <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+      <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+      </ul>
+    </header>
+  </div>
+<div class="container">
