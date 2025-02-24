c0e93861bc7f855828c3dd6e497046f0f780679c
sofiansaid <sofirm@live.nl>
2025-02-24 20:29:06 +0100

    sofian v1

--- app/views/header.php
+++ app/views/header.php
@@ -3,7 +3,7 @@
 <head>  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/4c23360f25.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="/style/style.css">
+  <link rel="stylesheet" href="/public/style/style.css">
   <title>TheFestival</title>
 </head>
 <body>
