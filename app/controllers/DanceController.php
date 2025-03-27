e01ae46aa65ffbeda827c8d6e3c374e80b9abc3e
AsielElaouare <elaouareasiel82@gmail.com>
2025-03-27 10:22:53 +0100

    commmiting dance cms example

--- app/controllers/DanceController.php
+++ app/controllers/DanceController.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Controllers;
 
+use App\Service\CmsService;
+
 class DanceController
 {
+    private $cmsService;
     public function index()
     {
+        $this->cmsService = new CmsService();
+        $blocks  = $this->cmsService->getPageById(5);
         // laad de dance page view
         require __DIR__ . '/../views/dance/dance.php';
     }
