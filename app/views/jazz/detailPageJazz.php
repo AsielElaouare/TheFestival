7160f800486db1191a3ee879b748599e5f333acd
Asiel <143839022+AsielElaouare@users.noreply.github.com>
2025-04-10 18:51:52 +0200

    small fixes

--- app/views/jazz/detailPageJazz.php
+++ app/views/jazz/detailPageJazz.php
@@ -4,21 +4,21 @@ include __DIR__ . '/../header.php';
 ?>
 <section class="flex bg-yellow">
     <img class="w-50" src="/uploads/jazz_pictures/haarlemJazz_vector.svg" >
-    <h6 class="fs-1 w-50 purple contenteditable" data-title="FirstSection">
+    <h1 class="fs-1 purple contenteditable text-center impact-font" data-title="FirstSection">
         <?= getContentByTitle($blocks, 'FirstSection') ?>
-    </h6>
+    </h1>
 </section>
 <section class="bg-yellow pt-5">
     <div class="position-relative">
         <img src="/uploads/bg_section1.svg" class="w-100" alt="">
         <div class="position-absolute  top-50 d-flex start-50 text-center translate-middle text-white">
-            <div class="flex">
+            <div class="d-flex flex-row">
                 <section class="contenteditable" data-title="imgBlock" >
-                    <img src="<?= getContentByTitle($blocks, 'imgBlock') ?: '' ?>" alt="">
+                    <img src="<?= getContentByTitle($blocks, 'imgBlock') ?: '' ?>" width="500rem" height="auto" alt="artist image">
                 </section>
-                <h1 class="impact-font contenteditable" data-title="SecondSection">
+                <p  class="h2 contenteditable yellow align-self-center ms-3" data-title="SecondSection">
                     <?= getContentByTitle($blocks, 'SecondSection') ?>
-                </h1>
+                </p>
             </div>
         </div>
     </div>
@@ -26,7 +26,7 @@ include __DIR__ . '/../header.php';
 <section class="bg-yellow pt-5">
     <div class="position-relative">
         <div class="text-center">
-            <h2 class="impact-font contenteditable" data-title="ThirdSection">
+            <h2 class="impact-font contenteditable purple" data-title="ThirdSection">
                 <?= getContentByTitle($blocks, 'ThirdSection') ?>
             </h2>
         </div>
